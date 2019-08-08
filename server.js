f51b15d3a29335b94eb0253d0354399d2cf89039
Nate Magera <nmagera07@gmail.com>
2019-08-08 15:08:34 -0400

    added some users to display

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express')
 
 const server = express()
 
+const users = [
+    {
+        user1: 'user1'
+    },
+    {
+        user2: 'user2'
+    },
+    {
+        user3: 'user3'
+    },
+
+]
+
 
 server.get('/', (req, res) => {
-    res.status(200).json({ message: 'hello you can see me now'})
+    res.status(200).json(users)
 })
 
 
